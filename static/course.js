178dd7d2f8e6a097cf1dec1f1a6c3ce90b85cebf
IL <irewlg@outlook.com>
2021-07-05 22:44:51 -0400

    update file

--- static/course.js
+++ static/course.js
@@ -20,15 +20,13 @@ function send_request(param) {
     $.ajax({
       method: 'GET',
       url: 'api/get_courses?' + param,
-      beforeSend: function() {
-        console.log('before send');
-      },
+      
       success: function(result) {
         disp(result);
         console.log('after send:' + result);
       },
       error: function() {
-        console.log('error');
+        console.log('ajax error');
       }
     });
 }
@@ -37,13 +35,12 @@ function send_request(param) {
 function disp(data) {
     let row;
     let page1 = '';
-    console.log('data: ' + data);
+   
     Object.keys(data).forEach(key => {
       elem = data[key];
-      console.log('elem: '+ elem);
       row = '<tr><td>' + elem['course_name'] + '</td>' + '<td>' + elem['start_date'] + '</td>' + '<td>' + elem['end_date'] + '</td>' + '<td>' + elem['location'] + '</td>' + '</tr>';
       page1 = page1 + row;
     });
-    console.log('page1: '+ page1)
+   
     $('#schTbl tbody').html(page1);
 }
